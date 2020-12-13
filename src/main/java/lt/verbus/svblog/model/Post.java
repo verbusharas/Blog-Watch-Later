5e2e46fc4dff71e9f00674d8593c10e93c0525c9
Sarunas Verbus <sarunas@verbus.lt>
2020-12-13 17:47:37 +0200

    update model to have type, imageUrl fields, expand body capacity to unlimited with @Lob annotation

--- src/main/java/lt/verbus/svblog/model/Post.java
+++ src/main/java/lt/verbus/svblog/model/Post.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -23,11 +24,13 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @ManyToOne
-    private User user;
-
     private String title;
 
+    private String type;
+
+    private String imageUrl;
+
+    @Lob
     private String body;
 
     @OneToMany
