c1a51683b3455693c136c45468d25b7622a1a634
Sarunas Verbus <sarunas@verbus.lt>
2020-12-13 17:46:44 +0200

    merge main pge (index) logic into one index controller

--- src/main/java/lt/verbus/svblog/controller/IndexController.java
+++ src/main/java/lt/verbus/svblog/controller/IndexController.java
@@ -1,14 +1,44 @@
 package lt.verbus.svblog.controller;
 
+import lt.verbus.svblog.model.Post;
+import lt.verbus.svblog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequestMapping
 public class IndexController {
 
-    @RequestMapping({"","/", "index", "index.html"})
-    public String index() {
+    private final PostService postService;
+
+    public IndexController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping({"","/", "index", "index.html"})
+    public String renderHome(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "index";
+    }
+
+    @GetMapping("/compose")
+    public String compose(Model model) {
+        model.addAttribute("post", new Post());
+        return "compose";
+    }
+
+    @PostMapping("/compose")
+    public String publish(@ModelAttribute("post") Post post, Model model) {
+        postService.save(post);
+        return "redirect:/index";
     }
 
 
