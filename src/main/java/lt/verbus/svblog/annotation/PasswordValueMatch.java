218b5b1519ad55620e48374fb175bab67d7e07b9
Sarunas Verbus <sarunas@verbus.lt>
2020-12-17 01:38:43 +0200

    used @PasswordValueMatch by aek

---
+++ src/main/java/lt/verbus/svblog/annotation/PasswordValueMatch.java
@@ -0,0 +1,39 @@
+package lt.verbus.svblog.annotation;
+
+import lt.verbus.svblog.util.PasswordFieldsValueMatchValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * <h2>PasswordValueMatch</h2>
+ *
+ * @author aek
+ */
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = PasswordFieldsValueMatchValidator.class)
+@Documented
+public @interface PasswordValueMatch {
+
+
+    String message() default "Fields values don't match!";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ ElementType.TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        PasswordValueMatch[] value();
+    }
+}
