ff4950c7fd5eb47c0000085244f80e1cb7f53347
Sarunas Verbus <sarunas@verbus.lt>
2020-12-17 01:43:43 +0200

    add user register validation

--- src/main/java/lt/verbus/svblog/controller/UserController.java
+++ src/main/java/lt/verbus/svblog/controller/UserController.java
@@ -4,10 +4,13 @@ import lt.verbus.svblog.model.User;
 import lt.verbus.svblog.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -26,7 +29,10 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute("user") User user){
+    public String registerNewUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
         userService.save(user);
         return "redirect:/private/user";
     }
