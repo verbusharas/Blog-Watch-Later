1f57d8c5007e8e6db46c80816afcd28731167c14
verbusharas <sarunas.verbus@gmail.com>
2020-12-17 17:05:23 +0200

    extend Default controller for session attribute sharing

--- src/main/java/lt/verbus/svblog/controller/IndexController.java
+++ src/main/java/lt/verbus/svblog/controller/IndexController.java
@@ -1,20 +1,22 @@
 package lt.verbus.svblog.controller;
 
 import lt.verbus.svblog.model.Post;
+import lt.verbus.svblog.model.User;
 import lt.verbus.svblog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping
-public class IndexController {
+public class IndexController extends DefaultController {
 
     private final PostService postService;
 
@@ -22,7 +24,7 @@ public class IndexController {
         this.postService = postService;
     }
 
-    @GetMapping({"","/", "index", "index.html"})
+    @GetMapping({"", "/", "index", "index.html"})
     public String renderHome(Model model) {
         List<Post> posts = postService.getAll();
         model.addAttribute("posts", posts);
