e194f1400e4af3ae2b66e0508eefbfcf7c281a82
verbusharas <sarunas.verbus@gmail.com>
2020-12-18 17:14:49 +0200

    minor clean-up

--- src/main/java/lt/verbus/svblog/controller/PostController.java
+++ src/main/java/lt/verbus/svblog/controller/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
