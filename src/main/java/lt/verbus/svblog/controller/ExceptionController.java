2ee9de6de571ffcd238e3b648600eb18837b4e73
Sarunas Verbus <sarunas@verbus.lt>
2020-12-19 20:18:10 +0200

    handle UserNotFoundException

--- src/main/java/lt/verbus/svblog/controller/ExceptionController.java
+++ src/main/java/lt/verbus/svblog/controller/ExceptionController.java
@@ -2,6 +2,7 @@ package lt.verbus.svblog.controller;
 
 import lt.verbus.svblog.exception.CommentNotFoundException;
 import lt.verbus.svblog.exception.PostNotFoundException;
+import lt.verbus.svblog.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,8 +17,13 @@ public class ExceptionController {
     }
 
     @ExceptionHandler(value = CommentNotFoundException.class)
-    public ResponseEntity<Object> commentException(PostNotFoundException exception) {
+    public ResponseEntity<Object> commentException(CommentNotFoundException exception) {
         return new ResponseEntity<>("Komentaras nerastas", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> userException(UserNotFoundException exception) {
+        return new ResponseEntity<>("Vartotojas nerastas", HttpStatus.NOT_FOUND);
     }
 
 }
