0d9c47af7151b0503484a15a9e85dcdf60657736
Sarunas Verbus <sarunas@verbus.lt>
2020-12-20 00:43:59 +0200

    minor clean-up

--- src/main/java/lt/verbus/svblog/controller/IndexController.java
+++ src/main/java/lt/verbus/svblog/controller/IndexController.java
@@ -1,19 +1,9 @@
 package lt.verbus.svblog.controller;
 
-import lt.verbus.svblog.model.Post;
-import lt.verbus.svblog.model.User;
 import lt.verbus.svblog.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-import java.util.Collections;
-import java.util.List;
 
 @Controller
 @RequestMapping
@@ -30,6 +20,5 @@ public class IndexController extends DefaultController {
         model.addAttribute("posts", postService.getAll());
         return "index";
     }
-
 
 }
