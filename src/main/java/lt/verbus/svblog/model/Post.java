8c4e0aad1d9782818f6953503433fb354e12f600
Sarunas Verbus <sarunas@verbus.lt>
2020-12-19 20:21:23 +0200

    add validation

--- src/main/java/lt/verbus/svblog/model/Post.java
+++ src/main/java/lt/verbus/svblog/model/Post.java
@@ -1,11 +1,15 @@
 package lt.verbus.svblog.model;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,12 +22,19 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "{post.title.not.blank}")
     private String title;
 
+    @NotBlank(message = "{post.type.not.blank}")
+    @Size(min=3, max = 30, message = "{post.type.size}")
     private String type;
 
+    @NotBlank(message = "{post.image-url.not.blank}")
+    @URL(message = "{post.image-url.url}")
     private String imageUrl;
 
+    @NotBlank(message = "{post.body.not.blank}")
+    @Size(min = 50, max = 950, message = "{post.body.size}")
     @Lob
     private String body;
 
