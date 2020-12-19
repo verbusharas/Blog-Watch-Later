db9219aa0d01eebae4e6e26dc90a894a4669a241
Sarunas Verbus <sarunas@verbus.lt>
2020-12-19 20:23:15 +0200

    add user promote/demote/delete/get services, add service for checking if username is unique

--- src/main/java/lt/verbus/svblog/service/UserService.java
+++ src/main/java/lt/verbus/svblog/service/UserService.java
@@ -1,5 +1,6 @@
 package lt.verbus.svblog.service;
 
+import lt.verbus.svblog.exception.UserNotFoundException;
 import lt.verbus.svblog.model.Role;
 import lt.verbus.svblog.model.User;
 import lt.verbus.svblog.repository.UserRepository;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 
@@ -31,12 +33,41 @@ public class UserService implements UserDetailsService {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
         user.setConfirmPassword(encodedPassword);
-        user.setRoles(Set.of(new Role(2L, "USER")));
+        user.setRoles(List.of(new Role(2L, "USER")));
         return userRepository.save(user);
+    }
+
+    public User promoteUserById(Long id){
+        User user = getUserById(id);
+        user.getRoles().clear();
+        user.getRoles().add(new Role(1L, "ADMIN"));
+        user.setConfirmPassword(user.getPassword());
+        return userRepository.save(user);
+    }
+
+    public User demoteUserById(Long id){
+        User user = getUserById(id);
+        user.getRoles().clear();
+        user.getRoles().add(new Role(2L, "USER"));
+        user.setConfirmPassword(user.getPassword());
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.delete(getUserById(id));
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    @Transactional
+    public boolean isUsernameUnique(String username) {
+        return userRepository.findByUsername(username).isEmpty();
     }
 }
