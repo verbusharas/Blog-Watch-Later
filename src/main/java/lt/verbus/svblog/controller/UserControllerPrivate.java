89ff050c5343bc0790c8775552dc30d527bafc01
Sarunas Verbus <sarunas@verbus.lt>
2020-12-20 00:46:13 +0200

    refactor admin methods to private user controller

--- src/main/java/lt/verbus/svblog/controller/UserControllerPrivate.java
+++ src/main/java/lt/verbus/svblog/controller/UserControllerPrivate.java
@@ -2,9 +2,11 @@ package lt.verbus.svblog.controller;
 
 
 import lt.verbus.svblog.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +25,24 @@ public class UserControllerPrivate extends DefaultController {
         model.addAttribute("users", userService.getAll());
         return "user/list";
     }
+
+    @GetMapping("/promote/{id}")
+    public String promoteUser(@PathVariable Long id, Model model){
+        userService.promoteUserById(id);
+        return "redirect:/private/user";
+    }
+
+    @GetMapping("/demote/{id}")
+    public String demoteUser(@PathVariable Long id, Model model){
+        userService.demoteUserById(id);
+        return "redirect:/private/user";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id, Model model){
+        userService.deleteUserById(id);
+        return "redirect:/private/user";
+    }
+
 
 }
