64844c3236786826953d568e9e79873f86e71ae4
Sarunas Verbus <sarunas@verbus.lt>
2020-12-19 20:20:05 +0200

    add user promote/demote/delete functionality

--- src/main/java/lt/verbus/svblog/controller/UserController.java
+++ src/main/java/lt/verbus/svblog/controller/UserController.java
@@ -1,5 +1,6 @@
 package lt.verbus.svblog.controller;
 
+import lt.verbus.svblog.model.Role;
 import lt.verbus.svblog.model.User;
 import lt.verbus.svblog.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -7,10 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/user")
@@ -24,16 +27,34 @@ public class UserController extends DefaultController {
 
     @GetMapping("/register")
     public String getUserRegistrationForm(Model model){
-        model.addAttribute("user", new User());
+        model.addAttribute("newUser", new User());
         return "user/register";
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+    public String registerNewUser(@ModelAttribute("newUser") @Valid User newUser, BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
             return "user/register";
         }
-        userService.save(user);
+        userService.save(newUser);
+        return "redirect:/private/user";
+    }
+
+    @GetMapping("/{id}/promote")
+    public String promoteUser(@PathVariable Long id, Model model){
+        userService.promoteUserById(id);
+        return "redirect:/private/user";
+    }
+
+    @GetMapping("/{id}/demote")
+    public String demoteUser(@PathVariable Long id, Model model){
+        userService.demoteUserById(id);
+        return "redirect:/private/user";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteUser(@PathVariable Long id, Model model){
+        userService.deleteUserById(id);
         return "redirect:/private/user";
     }
 
